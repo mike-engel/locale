078b9ad514f87dafc4517edb86140868e6607c87
Mike Engel <mike@mike-engel.com>
2019-02-07 00:44:55 -0700

    fix(now-v2): Better method to format the API for the body

--- src/api.rs
+++ src/api.rs
@@ -5,25 +5,17 @@ use serde_json::json;
 use std::error::Error;
 
 fn handler(request: Request) -> Result<impl IntoResponse, NowError> {
-    print!("Starting API request");
     let header = request.headers().get("Accept-Language");
     let parsed_headers = match header {
         Some(language_header) => parse(language_header.to_str().unwrap_or("")),
         None => vec![],
     };
-    print!("Building API response");
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(
-            json!(parsed_headers)
-                .as_str()
-                .expect("Unable to stringify JSON")
-                .to_owned(),
-        )
+        .body(format!("{}", json!(parsed_headers)))
         .expect("Internal Server Error");
 
-    print!("About to send API response");
     Ok(response)
 }
 
